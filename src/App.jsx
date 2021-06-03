135d1da24cda0bfba568fe4dcc5228e83d4ceeda
brenoangelo <brenoangeloal@gmail.com>
2021-06-02 22:25:06 -0300

    mexendo nos estilos...

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 import BodyTodo from './components/layout/BodyTodo'
-
+import Menu from './components/layout/Menu'
 export default () => (
-    <div id="app">
-        <BodyTodo>
-            <h1>Titulo</h1>
-        </BodyTodo>
+    <div className="app">
+
+        <header>
+            <div className="container">
+                <Menu />
+            </div>   
+        </header>
+ 
+        <div className="container">
+            <BodyTodo>
+                <h1>Preciso fazer...</h1>
+            </BodyTodo>
+        </div>
+        
     </div>
 )
---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default _ => (
+    <div className="menu">
+        <nav>
+        <span>Olá, Breno!</span>
+            <ul>
+                <li><a href="#">Meu progresso</a></li>
+            </ul>
+        </nav>
+    </div>
+)
